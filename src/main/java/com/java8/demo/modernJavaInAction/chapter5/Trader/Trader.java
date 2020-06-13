3a578324282736add95f9fbcdf79589c8992c558
kalkin <kalkin2@naver.com>
2020-06-13 13:05:28 +0900

    Trader.java add

---
+++ src/main/java/com/java8/demo/modernJavaInAction/chapter5/Trader/Trader.java
@@ -0,0 +1,14 @@
+package com.java8.demo.modernJavaInAction.chapter5.Trader;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Trader {
+    private String name;
+    private String city;
+
+
+}
